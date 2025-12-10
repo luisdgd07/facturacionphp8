638b1119f438be112508bf2d569aad824696e1dd
luisdgdd07 <luisdgd07@gmail.com>
2025-12-09 22:10:31 -0400

    favicon

--- core/modules/index/view/principal.php
+++ core/modules/index/view/principal.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="icon"
+        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📄</text></svg>">
     <?php
     $config = ConfigData::getAll();
     if (count($config) > 0) {
